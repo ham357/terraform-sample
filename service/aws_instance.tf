d0a8a8aacb5437feaef453d5c2b3c5ab5bf20706
ham357 <sakuraniumarete@gmail.com>
2019-06-24 01:01:50 +0900

    update service

--- service/aws_instance.tf
+++ service/aws_instance.tf
@@ -1,10 +1,18 @@
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = "${aws_lb.lb.arn}"
-  port              = "80"
-  protocol          = "HTTP"
+resource "aws_instance" "sample" {
+  ami                         = "ami-05b296a384694dfa4"
+  instance_type               = "t2.small"
+  monitoring                  = true
+  iam_instance_profile        = "${data.terraform_remote_state.aws_iam.outputs.ecs_instance_profile_name}"
+  subnet_id                   = "${data.terraform_remote_state.vpc.outputs.public_subnet_1_id}"
+  user_data                   = "${data.template_file.user_data.rendered}"
+  associate_public_ip_address = true
 
-  default_action {
-    target_group_arn = "${aws_lb_target_group.http.arn}"
-    type             = "forward"
+  vpc_security_group_ids = [
+    "${aws_security_group.instance.id}",
+  ]
+
+  root_block_device {
+    volume_size = "20"
+    volume_type = "gp2"
   }
 }
