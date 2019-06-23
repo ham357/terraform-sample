7154824e9839fb4b427082e7d6bf789fe2ac6e4f
ham357 <sakuraniumarete@gmail.com>
2019-06-23 23:16:08 +0900

    update vpc

--- vpc/config.tf
+++ vpc/config.tf
@@ -1,10 +1,10 @@
 terraform {
-    backend "s3" {
-      bucket = "ham357-sample-terraform"
-      key    = "sample/vpc/terraform.tfstate"
-      region = "ap-northeast-1"
-    } 
- }
+  backend "s3" {
+    bucket = "ham357-sample-terraform"
+    key    = "sample/vpc/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
 
 provider "aws" {
   region = "ap-northeast-1"
